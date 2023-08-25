9677b760f2ff7dcac2ff649e8566c7a57e9fda54
p21nc3 <pp4768@srmist.edu.in>
2023-08-26 04:38:04 +0530

    FIXED history & Ops format

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const app = express();
 
-const operators = ['+', '-', '*', '/']; // aur ops add kryo
+const operators = ['+', '-', '*', '/'];
 
 const history = [];
-
 app.get('/', (req, res) => {
   res.send(operators);
 });
-app.get('/history', (req, res) => {
-        // history figure out krna h
-});
-app.get('/:fnumber/:operator/:snumber', (req, res) => {
 
+app.get('/history', (req, res) => {
+  const recentHistory = history.slice(-20);
+  res.send(recentHistory);
+});
+
+app.get('/:fnumber/:operator/:snumber', (req, res) => {
+  const fnumber = req.params.fnumber;
+  const operator = req.params.operator;
+  const snumber = req.params.snumber;
+  const answer = calculate(fnumber, operator, snumber);
+  history.push({
+    question: `${fnumber} ${operator} ${snumber}`,
+    answer: answer
+  });
+  res.send({
+    question: `${fnumber} ${operator} ${snumber}`,
+    answer: answer
+  });
 });
 
 function calculate(fnumber, operator, snumber) {
+  if (!operators.includes(operator)) {
+    throw new Error(`Invalid operator: ${operator}`);
+  }
+  const nums = [Number(fnumber), Number(snumber)];
+  switch (operator) {
+    case '+':
+      return nums.reduce((a, b) => a + b);
+    case '-':
+      return nums.reduce((a, b) => a - b);
+    case '*':
+      return nums.reduce((a, b) => a * b);
+    case '/':
+      return nums[0] / nums[1];
+    default:
+      throw new Error(`Invalid operator: ${operator}`);
+  }
 }
 
 app.listen(3000, () => {
